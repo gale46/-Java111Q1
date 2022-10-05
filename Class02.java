ea6a17d7fa1bee74255e2115ee879f226ee93c6f
gale46 <113490808+gale46@users.noreply.github.com>
2022-10-05 17:58:05 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -1,32 +1,46 @@
+import java.text.Format;
+
 class Data {
-	private String name;
-	private Test score;
-	int eng, m;
+    private String name;
+    private Test score;
+    int eng, m;
+    double eng_f,m_f;
 
-	void Test(int eng, int m) {
-		this.eng = eng;
-		this.m = m;
-	}
+    void per_da(String name) {
+        this.name = name;
+    }
 
-	class Test {
-		double avg() {
-			return (eng + m) / 2;
-		}
+    void Test(int eng, int m) {
+        this.eng = eng;
+        this.m = m;
+        eng_f = Double.valueOf(eng);
+        m_f = Double.valueOf(m);
+    }
 
-		
-	}
-	public void show() {
-		System.out.println(eng + m + avg());
-	}
+    class Test {
+        double avg() {
+            Double ans = (eng_f + m_f) / 2;
+            return ans;
+        }
+
+        public void show() {
+            System.out.printf("name:%s english:%d math%d: avg:%.1f\n", name, eng, m, avg());
+        }
+
+    }
 
 }
 
 public class Class02 {
-	public static void main(String[] args) {
-		Data da = new Data();
-		da.Test(60, 70);
-		
-		
-		
-	}
+    public static void main(String[] args) {
+        Data da = new Data();
+        da.per_da("Annie");
+        da.Test(85, 92);
+        Data.Test show = da.new Test();
+        show.show();
+        da.per_da("Brian");
+        da.Test(77, 56);
+        show.show();
+
+    }
 }
